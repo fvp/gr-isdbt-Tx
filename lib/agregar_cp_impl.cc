cb1f4facbf22311a5736d2de7ea7e2cf53e04a4f
Javier Hernandez <javiherbar7@gmail.com>
2017-10-13 20:34:49 -0300

    Bloque CP
    Se agrego el bloque CP a proyecto, pero falta cambiar el comportamiento y los casos de test.

---
+++ lib/agregar_cp_impl.cc
@@ -0,0 +1,89 @@
+/* -*- c++ -*- */
+/*  
+ * Copyright 2015, 2016, 2017
+ *   Federico "Larroca" La Rocca <flarroca@fing.edu.uy>
+ *   Pablo Belzarena 
+ *   Gabriel Gomez Sena 
+ *   Pablo Flores Guridi 
+ *   Victor Gonzalez Barbone
+ * 
+ *   Instituto de Ingenieria Electrica, Facultad de Ingenieria,
+ *   Universidad de la Republica, Uruguay.
+ *  
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *  
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *  
+ * You should have received a copy of the GNU General Public License
+ * along with this software; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ * 
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <gnuradio/io_signature.h>
+#include "agregar_cp_impl.h"
+
+namespace gr {
+  namespace isdbt {
+
+    agregar_cp::sptr
+    agregar_cp::make()
+    {
+      return gnuradio::get_initial_sptr
+        (new agregar_cp_impl());
+    }
+
+    /*
+     * The private constructor
+     */
+    agregar_cp_impl::agregar_cp_impl()
+      : gr::block("agregar_cp",
+              gr::io_signature::make(<+MIN_IN+>, <+MAX_IN+>, sizeof(<+ITYPE+>)),
+              gr::io_signature::make(<+MIN_OUT+>, <+MAX_OUT+>, sizeof(<+OTYPE+>)))
+    {}
+
+    /*
+     * Our virtual destructor.
+     */
+    agregar_cp_impl::~agregar_cp_impl()
+    {
+    }
+
+    void
+    agregar_cp_impl::forecast (int noutput_items, gr_vector_int &ninput_items_required)
+    {
+      /* <+forecast+> e.g. ninput_items_required[0] = noutput_items */
+    }
+
+    int
+    agregar_cp_impl::general_work (int noutput_items,
+                       gr_vector_int &ninput_items,
+                       gr_vector_const_void_star &input_items,
+                       gr_vector_void_star &output_items)
+    {
+      const <+ITYPE+> *in = (const <+ITYPE+> *) input_items[0];
+      <+OTYPE+> *out = (<+OTYPE+> *) output_items[0];
+
+      // Do <+signal processing+>
+      // Tell runtime system how many input items we consumed on
+      // each input stream.
+      consume_each (noutput_items);
+
+      // Tell runtime system how many output items we produced.
+      return noutput_items;
+    }
+
+  } /* namespace isdbt */
+} /* namespace gr */
+
