8f5fa12a7e7cde71efd34ec866c13af1536ea6b6
Unknown <javiherbar7@gmail.com>
2018-03-13 18:24:41 -0400

    Working on Full Seg Frec_interleaver
    Developing inter segment randomizer.

--- lib/frec_interleaver_impl.cc
+++ lib/frec_interleaver_impl.cc
@@ -66,6 +66,7 @@ namespace gr {
       d_mode = mode; 
       d_carriers_per_segment = d_data_carriers_mode1*((int)pow(2.0,mode-1)); 
       d_noutput = d_total_segments*d_carriers_per_segment;
+      d_IsFullSeg = IsFullSeg;
       }
 
     /*
@@ -87,8 +88,7 @@ namespace gr {
       gr_complex data[d_total_segments][d_carriers_per_segment] = {}; 
       gr_complex data_interleaved[d_total_segments][d_carriers_per_segment] = {};
       gr_complex out_before_rand[d_noutput] = {};
-
-      int k=0;
+      int k = 0;
 
       if (!d_IsFullSeg)
       {
@@ -98,7 +98,8 @@ namespace gr {
       }
 
       //1. Intra-Segment interleaving
-      printf("1. Intra-Segment interleaving\n");  
+      printf("1. Intra-Segment interleaving\n"); 
+      k=0; 
       for(int i=0; i<d_total_segments; i++){
         for (int j=0; j<d_carriers_per_segment; j++){
           // First we write the matrix rows-first
@@ -106,7 +107,13 @@ namespace gr {
           k++;
         }
       }   
-      
+      for (int j=0; j<d_total_segments; j++){
+        for(int i=0; i<d_carriers_per_segment; i++){
+          // First we write the matrix rows-first
+          data_interleaved[d_total_segments -j -1][d_carriers_per_segment - i -1] = data[j][i];
+          printf("data_interleaved[%i][%i] = %f \n", i, j, data_interleaved[i][j].real());
+        }
+      } 
       
       //2. Inter-segment rotation
       k = 0;
@@ -114,14 +121,23 @@ namespace gr {
         for (int j=0; j<d_carriers_per_segment; j++){
           // First we re-write the data matrix, rows-first
           data_interleaved[i][j] = data[i][(i + j) % d_carriers_per_segment];
-          out_before_rand[k] = data_interleaved[i][j];
-          k++;
+          printf("Numero magico %i \n", (i + j) % d_carriers_per_segment);
+          printf("data_interleaved[%i][%i] = %f \n", i, j, data_interleaved[i][j].real());
         }
       }
+      for(int i=0; i<d_total_segments; i++){
+        for (int j=0; j<d_carriers_per_segment; j++){
+          // First we write the matrix rows-first
+          out_before_rand[k] = data_interleaved[i][j];
+          k++;
+          //printf("out_before_rand[%i] = %f \n", k, out_before_rand[k].real());
+        }
+      } 
       printf("2. Inter-segment rotation\n");
 
       //3. Inter-segment randomizer
 Randomizer:
+      int index = 0;
       printf("3. Inter-segment randomizer\n");
       switch(d_mode)
       {
@@ -129,10 +145,9 @@ Randomizer:
         {
           for(int i=0; i<d_noutput; i++)
           {
-            int index = rand_mode_1[i];
+            index = rand_mode_1[i];
             out[index] = out_before_rand[i];
-            printf("in[%i] = %f \t out_before_rand[%i] = %f \t", i, in[i].real(), i, out_before_rand[i].real());  
-            printf("out[%i] = %f \n", i, out[i].real());  
+            //printf("out[%i] = %f \n", index, out[index].real());
           }
           break;
         }
@@ -140,8 +155,8 @@ Randomizer:
         {
           for(int i=0; i<d_noutput; i++)
           {
-            int index = rand_mode_2[i];
-            out[i] = out_before_rand[index];
+            index = rand_mode_2[i];
+            out[index] = out_before_rand[i];
           }
           break;
         }
@@ -149,8 +164,8 @@ Randomizer:
         {
           for(int i=0; i<d_noutput; i++)
           {
-            int index = rand_mode_3[i];
-            out[i] = out_before_rand[index];
+            index = rand_mode_3[i];
+            out[index] = out_before_rand[i];
           }
           break;
         }
@@ -161,7 +176,7 @@ Randomizer:
         }
       }
 
-
+      this->consume(0, noutput_items);
       // Tell runtime system how many output items we produced.
       return noutput_items;
     }
